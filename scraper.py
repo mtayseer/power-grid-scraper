c0d8b7f13a74fd4da7b36d30a61224b76367acbe
Mohammad Tayseer <m.tayseer82@gmail.com>
2014-08-10 19:46:19 +0200

    Use Mosab's site to get the status

--- scraper.py
+++ scraper.py
@@ -1,8 +1,9 @@
 import urllib, datetime, os
 
 def fetch():
-    url = 'http://loadmeter.egyptera.org/ClockToolTip.aspx'
-    output = datetime.datetime.now().strftime('egyptera.%Y-%m-%d-%H-%M-%S.html')
+    # Instead of doing all the parsing later, I get the status from Mosab's site & store it
+    url = 'http://power-grid-status.mos3abof.com/status'
+    output = datetime.datetime.now().strftime('egyptera.%Y-%m-%d-%H-%M-%S.json')
     output = os.path.join(os.path.dirname(__file__), output)
     content = urllib.urlretrieve(url, output)
 
